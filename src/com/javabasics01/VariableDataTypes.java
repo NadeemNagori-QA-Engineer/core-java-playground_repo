80cb2414373947899ca7ed44bcd31e06dc33b7f7
Nadeem Nagori <m.nadeemnagori@gmail.com>
2026-06-13 20:00:23 +0530

    Added VariablesDemo class

---
+++ src/com/javabasics01/VariableDataTypes.java
@@ -0,0 +1,40 @@
+package com.javabasics01;
+
+public class VariableDataTypes {
+public static void main(String[] args) {
+/*	Topic 2: Variables and Data Types
+	Variable is a container which is hold a value in a memory.
+	Java me Data Types do tarah ke hote hain:*/
+
+//	1. Primitive Data Types (basic types):
+/*	Data Type		Size		Example
+ 	
+ 	int				4bytes		whole numbers: 25, -10
+ 	double			8bytes		Decimal Numbers: 3.14, 99.32
+ 	char			2bytes		Single Character: 'A', '5'
+ 	boolean			1bit		true/false
+ 	long			8bytes		Bade whole numbers
+ 	float			4bytes		chotedecimal numbers
+ 	
+ 	Example of Variable Declaration
+ 	dataType variableName = value;
+ 	int num = 10;
+ */	
+	
+//	2. Non-Primitive (Reference) Data Types: String, Arrays, Classes, etc.
+	
+	int age = 28;
+	double salary = 450000.50;
+	char grad = 'A';
+	boolean isJavaFun = true;
+	String name ="Nadeem";
+	
+	System.out.println(age);
+	System.out.println(salary);
+	System.out.println(grad);
+	System.out.println(isJavaFun);
+	System.out.println(name);
+	
+	
+}
+}
